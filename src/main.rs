3e5a63be05ffb0a7101feccd656b29678c11d2b8
Valery Kostikov <mkoctuk@gmail.com>
2019-04-22 07:29:29 +0300

    add config

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
+mod db;
 mod netutils;
 
+struct Config {
+    db: String,
+    target: String
+}
+
+fn get_config<'a>() -> Config {
+    let db = dotenv::var("db")
+        .expect("No found variable db_uri like postgres://postgres@localhost:5433 in environment");
+        let target = dotenv::var("db")
+        .expect("No found variable target like http://targethost:433/path in environment");
+        Config{db, target}
+}
+
 fn main() -> Result<(), reqwest::Error> {
+    let config = get_config();
     let proxy = reqwest::Proxy::all("socks5h://192.168.31.1:9050")?;
     let client: reqwest::Client = reqwest::Client::builder().proxy(proxy).build()?;
     let resp = client.get("http://udds.ru:16016/c").send()?.text()?;
