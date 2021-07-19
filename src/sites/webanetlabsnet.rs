a6bc3c9ce9b86bc4bd4064efc15bdbe7723ee091
Serbe <mkoctuk@gmail.com>
2021-07-19 16:14:11 +0300

    fix links

--- src/sites/webanetlabsnet.rs
+++ src/sites/webanetlabsnet.rs
@@ -4,14 +4,13 @@ use regex::Regex;
 
 pub async fn get() -> Result<Vec<String>, RspError> {
     let body = crawl("https://webanetlabs.net/publ/24").await?;
-    let re_url = Regex::new(r#"href="(/freeproxyweb/proxylist_at_\d{2}\.\d{2}.\d{4}.txt)"#)?;
+    let re_url = Regex::new(r#"href="(/proxylist.*\.html)"#)?;
     let re = Regex::new(r"(\d{2,3}\.\d{2,3}\.\d{2,3}\.\d{2,3}:\d{2,4})")?;
     let mut list = Vec::new();
     let links: Vec<String> = re_url
         .captures_iter(&body)
         .map(|cap| format!("https://webanetlabs.net/{}", &cap[1]))
         .collect();
-    dbg!(&links);
     for link in links {
         let body = crawl(&link).await?;
         list.append(
